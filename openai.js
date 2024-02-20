a6ce82d8d0f5b798013e2f5c28c1553201eaef7c
Decem6er <cgmz@live.com>
2024-02-20 18:51:52 +0800

    Add Models

--- openai.js
+++ openai.js
@@ -1,7 +1,7 @@
 
 import { Configuration, OpenAIApi } from "openai";
 
-const models = ['text-davinci-003','code-davinci-002','gpt-3.5-turbo'];
+const models = ['text-davinci-003','code-davinci-002','gpt-3.5-turbo','gpt-4','gemini-pro'];
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
